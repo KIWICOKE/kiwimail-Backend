7820181a801193b52fc77730f7f84f3428857546
PADO <112324554+padoz3@users.noreply.github.com>
2024-01-10 00:01:27 +0900

    Add files via upload

--- kiwimail/kiwimail/urls.py
+++ kiwimail/kiwimail/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/user/', include('allauth.urls')),
     path('api/user/', include('user.urls')),
     path('login/', views.google_login, name='google_login'),
-    path('insta/', views.insta)
+    path('insta/', views.insta),
+    path('<int:receiver_id>/', include('post.urls'))
 ]
 
