0d8fa5f731439047c8429e65d998432bfe9ed088
PADO <112324554+padoz3@users.noreply.github.com>
2024-01-21 18:09:00 +0900

    Update urls.py
    method로 편지 작성하기, 편지 목록 불러오기 구분하도록 수정

--- kiwimail/letter/urls.py
+++ kiwimail/letter/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from letter.views import MessageCreateView, MessageDetailView
+from letter.views import MessageCreateListView, MessageDetailView
 
 app_name = 'letter'
 
 urlpatterns = [
-    path('create/', MessageCreateView.as_view(template_name = 'letter/create.html'), name='create'),
-    path('list/', TemplateView.as_view(template_name = 'letter/list.html'), name='list'),
-    path('detail/', MessageDetailView.as_view(template_name = 'letter/detail.html'), name='detail'),
+    path('', MessageCreateListView.as_view(), name='create_or_list'),
+    path('<int:pk>/', MessageDetailView.as_view(template_name='letter/detail.html'), name='detail'),
 ]
