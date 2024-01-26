52fe799df0d82d2922c2e6609c8b39f70fc1ba69
PADO <112324554+padoz3@users.noreply.github.com>
2024-01-27 02:55:01 +0900

    Delete padoz3 directory

--- padoz3/kiwiletter/letter/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from letter import views
-from . import views
-
-urlpatterns = [
-    path('', views.write, name='write'),
-    path('<int:post_id>/', views.letter, name='check'),
-]
